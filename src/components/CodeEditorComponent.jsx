b2e9a437c2fe38f210fce5da8f03b0b172a9fa20
slaw469 <160684826+slaw469@users.noreply.github.com>
2025-03-26 19:08:23 -0500

    Update CodeEditorComponent.jsx

--- src/components/CodeEditorComponent.jsx
+++ src/components/CodeEditorComponent.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 
-// Import your technology icons
+// Import technology icons
 import {
   html,
   css,
